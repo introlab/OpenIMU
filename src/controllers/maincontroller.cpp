747672bf666532eb02c48a3d7d8cb1eb1310ec01
Michael Deslauriers <michael.deslauriers-roy@usherbrooke.ca>
2016-02-16 16:39:54 -0500

    Added dynamic layout

---
+++ src/controllers/maincontroller.cpp
@@ -0,0 +1,8 @@
+#include "maincontroller.h"
+#include "models/viewloader.h"
+
+MainController::MainController(MainWindow& mainWindow)
+{
+    ViewLoader loader = ViewLoader();
+    loader.loadLayout(mainWindow);
+}
--- src/views/mainwindow.cpp
+++ src/views/mainwindow.cpp
@@ -6,9 +6,16 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    gridLayout = new QGridLayout();
+    this->centralWidget()->setLayout(gridLayout);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::AddCustomWidget(QWidget* widget, int x, int y)
+{
+    this->gridLayout->addWidget(widget,x,y);
 }
