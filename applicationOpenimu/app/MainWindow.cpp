2eb6c1edf9e507a093d8de2221337f586313e6cb
Marc-Antoine Depelteau <olivier.charbonneau@usherbrooke.ca>
2016-10-18 15:50:53 -0400

    OP-313: Part 2: Create TableWidget for algorithms (MADO) Merge branch 'develop' of https://github.com/introlab/OpenIMU into develop

--- applicationOpenimu/app/MainWindow.cpp
+++ applicationOpenimu/app/MainWindow.cpp
@@ -3,6 +3,7 @@
 #include <QListWidgetItem>
 #include<vector>
 #include<QDebug>
+#include "widgets/AlgorithmTab.h"
 
 #include "AccDataDisplay.h"
 #include "QMessageBox"
@@ -68,42 +69,10 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent)
     homeLayout->addSpacing(100);
     homeLayout -> setAlignment(homeLabel,Qt::AlignCenter);
     tabWidget->addTab(homeWidget,tr("Accueil"));
-/*
-    // -- Algorithm Tab -- TODO: MOVE BY MADO
-    QWidget * algorithmWidget = new QWidget(); //To do create classe Home widget
-    QVBoxLayout* algorithmLayout = new QVBoxLayout(algorithmWidget);
 
-    // -- Algorithm Section
-    QLabel *algorithmLabel = new QLabel(tr("Algorithms"));
-
-    QTableView *algorithmTableView = new QTableView(this);
-   // connect(algorithmTableView, SIGNAL(itemClicked(QTableViewItem*)),this, SLOT(onListItemClicked(QTableViewItem*)));
-
-    //algorithmTableView->setAlternatingRowColors(true);
-    //algorithmTableView->setStyleSheet("alternate-background-color:#ecf0f1;background-color:white;");
-
-    QStandardItemModel *algorithmModel = new QStandardItemModel(this);
-    algorithmModel->setHeaderData(0, Qt::Horizontal, QObject::tr("ID"));
-    algorithmModel->setHeaderData(1, Qt::Horizontal, QObject::tr("Name"));
-    algorithmModel->setHeaderData(2, Qt::Horizontal, QObject::tr("Description"));
-    algorithmModel->setHeaderData(3, Qt::Horizontal, QObject::tr("Author"));
-
-    algorithmTableView->setModel(algorithmModel);
-    algorithmTableView->show();
-
-    // -- Parameter Section
-    QLabel *parameterLabel = new QLabel(tr("Parameter(s)"));
-
-    algorithmLayout->addWidget(algorithmLabel);
-    algorithmLayout->addWidget(algorithmTableView);
-    algorithmLayout->addWidget(parameterLabel);
-    algorithmWidget->setLayout(algorithmLayout);
-
+    QWidget * algorithmWidget = new AlgorithmTab(this);
     tabWidget->addTab(algorithmWidget,tr("Algorithmes"));
 
-    // -- END OF Algorithm Tab -- TODO: MOVE BY MADO
-
-*/
     tabWidget->setCurrentWidget(tabWidget->widget(0));
     tabWidget->grabGesture(Qt::PanGesture);
     tabWidget->grabGesture(Qt::PinchGesture);
@@ -210,7 +179,7 @@ void MainWindow::reponseRecue(QNetworkReply* reply)
     if (reply->error() == QNetworkReply::NoError)
    {
        qDebug() << "connection main";
-       std::string testReponse = reply->readAll();
+       std::string testReponse(reply->readAll());
        record.m_WimuRecordList.clear();
        CJsonSerializer::Deserialize(&record, testReponse);
 
--- applicationOpenimu/app/widgets/AlgorithmTab.h
+++ applicationOpenimu/app/widgets/AlgorithmTab.h
@@ -1,6 +1,10 @@
 #ifndef ALGORITHMTAB_H
 #define ALGORITHMTAB_H
 
+#include "MainWidget.h"
+#include "QLabel"
+#include "QStandardItemModel"
+#include "QTableWidget"
 #include <QWidget>
 
 class AlgorithmTab : public QWidget
@@ -9,10 +13,21 @@ class AlgorithmTab : public QWidget
 public:
     explicit AlgorithmTab(QWidget *parent = 0);
 
+private:
+    QVBoxLayout * algorithmLayout;
+
+    // -- Algorithm List Section
+    QLabel * algorithmLabel;
+    QTableWidget * algorithmTableWidget;
+    QStringList algorithmTableHeaders;
+
+    // -- Parameter Section
+    QLabel * parameterLabel;
+
 signals:
 
 public slots:
-
+    void openParametersWindow(const QModelIndex &index);
 };
 
 #endif // ALGORITHMTAB_H
