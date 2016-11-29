76fed049f16c281685cfdd91a67c1a1bdfbe5425
Stephane Diallo <stephdia2343@gmail.com>
2016-11-29 13:57:46 -0500

    BUGFIX delete record

--- PythonAPI/lib/lib_openimu/resources.py
+++ PythonAPI/lib/lib_openimu/resources.py
@@ -193,7 +193,7 @@ class DeleteData(Resource):
         res1 = mongo.db.accelerometres.delete_many({'ref':uuid})
         res2 = mongo.db.gyrometres.delete_many({'ref':uuid})
         res3 = mongo.db.magnetometres.delete_many({'ref':uuid})
-        res4 = mongo.db.record.delete_many({'_id':uuid})
+        res4 = mongo.db.record.delete_many({'_id':ObjectId(uuid)})
         result = res1.deleted_count+res2.deleted_count+res3.deleted_count+res4.deleted_count
         return 'Affected ' + str(result)     + ' entries.'
 
