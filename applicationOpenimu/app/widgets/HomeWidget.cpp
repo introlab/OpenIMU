3b74b1a5c62ef68bee58fba14409c78606760431
Stephane Diallo <stephdia2343@gmail.com>
2016-12-05 15:56:56 -0500

    OP-360:Change record date above graph in french + github link in home widget

--- applicationOpenimu/app/widgets/HomeWidget.cpp
+++ applicationOpenimu/app/widgets/HomeWidget.cpp
@@ -1,5 +1,6 @@
 #include"HomeWidget.h"
 #include "ui_homewidget.h"
+#include <QDesktopServices>
 
 HomeWidget::HomeWidget(QWidget *parent) :
     QWidget(parent),
@@ -7,9 +8,17 @@ HomeWidget::HomeWidget(QWidget *parent) :
 {
     ui->setupUi(this);
     m_parent = parent;
+    connect(ui->gitbtn, SIGNAL(clicked()), this, SLOT(openGitLink()));
+
 }
 
 HomeWidget::~HomeWidget()
 {
     delete ui;
+}
+
+void HomeWidget::openGitLink()
+{
+    QString link = "https://github.com/introlab/OpenIMU";
+    QDesktopServices::openUrl(QUrl(link));
 }
