99cf267326b2074845a2f0f01e72e4bc0b16dc1c
Dominic Létourneau <doumdi@gmail.com>
2017-03-30 10:46:29 -0400

    trying docker and automated deployment

--- PythonAPI/lib/lib_openimu/conf.py
+++ PythonAPI/lib/lib_openimu/conf.py
@@ -1,9 +1,10 @@
 import simplejson
+import os
 
 ERROR_404_HELP = False      # Don't add extra suggestions to 404 error msg
 RESTFUL_JSON = {'cls': simplejson.JSONEncoder}
 
-MONGO_HOST = "localhost"
+MONGO_HOST = os.getenv('OPENIMU_DATABASE_SERVER', 'localhost')
 MONHO_PORT = 27017
 MONGO_DBNAME = "test"
 
--- PythonAPI/lib/lib_openimu/shared.py
+++ PythonAPI/lib/lib_openimu/shared.py
@@ -1,7 +1,4 @@
 from flask_marshmallow import Marshmallow
 from flask_pymongo import PyMongo
-import os
-
 ma = Marshmallow()
-
-mongo = PyMongo(os.getenv('OPENIMU_DATABASE_SERVER', 'localhost'),27017)
+mongo = PyMongo()
