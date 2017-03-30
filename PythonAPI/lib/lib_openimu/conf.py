8c5b3b19ccd8614483fb65eab682afe189c523e0
Dominic Létourneau <doumdi@gmail.com>
2017-03-30 10:48:37 -0400

    trying docker and automated deployment

--- PythonAPI/lib/lib_openimu/conf.py
+++ PythonAPI/lib/lib_openimu/conf.py
@@ -6,7 +6,7 @@ RESTFUL_JSON = {'cls': simplejson.JSONEncoder}
 
 MONGO_HOST = os.getenv('OPENIMU_DATABASE_SERVER', 'localhost')
 MONHO_PORT = 27017
-MONGO_DBNAME = "test"
+MONGO_DBNAME = "openimu"
 
 VERSION_MAJOR = 0
 VERSION_MINOR = 1
