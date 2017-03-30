932d70dd1462ccd39cbd55cc62bc119fa38750d4
Dominic Létourneau <doumdi@gmail.com>
2017-03-30 10:38:59 -0400

    trying docker and automated deployment

--- PythonAPI/lib/lib_openimu/shared.py
+++ PythonAPI/lib/lib_openimu/shared.py
@@ -1,4 +1,7 @@
 from flask_marshmallow import Marshmallow
 from flask_pymongo import PyMongo
+import os
+
 ma = Marshmallow()
-mongo = PyMongo()
+
+mongo = PyMongo(os.getenv('OPENIMU_DATABASE_SERVER', 'localhost'),27017)
