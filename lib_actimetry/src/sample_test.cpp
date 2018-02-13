5748d9f1efc79ccd3248b0538afb6901efec6581
Dominic Létourneau <doumdi@gmail.com>
2018-02-13 11:20:51 -0500

    added GSL

--- lib_actimetry/src/sample_test.cpp
+++ lib_actimetry/src/sample_test.cpp
@@ -3,10 +3,8 @@
 
 
 #include <math.h>
-
-#if 0
 #include "kfr/all.hpp"
-#endif
+
 
 
 #include "liquid.h"
@@ -20,9 +18,9 @@ namespace {
       EXPECT_EQ(0, myString.size());
     }
 
-#if 0
+#if 1
     //KFRTest1
-    TEST(KFR, SimpleTest1) {
+    TEST(KFR, Resample) {
 
         //Will use namespace kfr
         using namespace kfr;
@@ -42,7 +40,7 @@ namespace {
         univector<fbase> resampled(o1.size() * output_sr / input_sr);
 
         //Resampler
-        auto r = resampler<fbase>(resample_quality::low, output_sr, input_sr, 1.0, 0.496);
+        auto r = resampler<fbase>(resample_quality::low, output_sr, input_sr, 1.0, 0.49);
 
         println("output_signal");
         //Resample & get size
@@ -53,8 +51,81 @@ namespace {
     }
 #endif
 
+#if 0
     //LIQUID
-    TEST(LIQUID, SimpleTest1) {
+    TEST(LIQUID, Interpolate) {
+        // options
+        unsigned int k=4;                   // samples/symbol
+        unsigned int m=3;                   // filter delay
+        float As = 60.0f;                   // filter stop-band attenuation
+        unsigned int num_data_symbols=100;   // number of data symbols
+
+        // derived values
+        unsigned int num_symbols = num_data_symbols + 2*m;  // compensate for filter delay
+        unsigned int num_samples = k*num_symbols;
+
+
+
+        float input_signal_sine_1hz_100[100] = {
+            0, 0.0627905,  0.125333,  0.187381,   0.24869,  0.309017,  0.368125,   0.42578,
+             0.481754,  0.535827,  0.587786,  0.637425,  0.684548,  0.728969,  0.770513,  0.809017,
+             0.844327,  0.876306,  0.904826,  0.929776,  0.951056,  0.968583,  0.982288,  0.992116,
+             0.998027,  0.999999,  0.998027,  0.992116,  0.982288,  0.968583,  0.951056,  0.929776,
+             0.904826,  0.876306,  0.844327,  0.809017,  0.770513,  0.728969,  0.684548,  0.637425,
+             0.587786,  0.535827,  0.481754,   0.42578,  0.368125,  0.309017,   0.24869,  0.187381,
+             0.125333, 0.0627905,         0,    -0.063, -0.125333, -0.187381,  -0.24869, -0.309017,
+            -0.368125,  -0.42578, -0.481754, -0.535827, -0.587786, -0.637425, -0.684548, -0.728969,
+            -0.770513, -0.809017, -0.844327, -0.876306, -0.904826, -0.929776, -0.951056, -0.968583,
+            -0.982288, -0.992116, -0.998027, -0.999999, -0.998027, -0.992116, -0.982288, -0.968583,
+            -0.951056, -0.929776, -0.904826, -0.876306, -0.844327, -0.809017, -0.770513, -0.728969,
+            -0.684548, -0.637425, -0.587786, -0.535827, -0.481754,  -0.42578, -0.368125, -0.309017,
+             -0.24869, -0.187381, -0.125333,    -0.063 };
+
+
+        liquid_float_complex x[num_symbols];   // input symbols
+        liquid_float_complex y[num_samples];   // output samples
+
+
+        memset(x,0,num_symbols * sizeof(liquid_float_complex));
+        memset(y,0,num_samples * sizeof(liquid_float_complex));
+
+        for (auto i = 0; i < num_data_symbols; i++)
+        {
+            x[i].real = input_signal_sine_1hz_100[i];
+            x[i].imag = 0;
+        }
+
+        // create interpolator from prototype
+        firinterp_crcf q = firinterp_crcf_create_kaiser(k,m,As);
+
+        // interpolate symbols
+        for (auto i=0; i<num_symbols; i++)
+              firinterp_crcf_execute(q, x[i], &y[k*i]);
+
+
+        // destroy interpolator object
+        firinterp_crcf_destroy(q);
+
+        // print results to screen
+        printf("x(t) :\n");
+        for (auto i=0; i<num_symbols; i++)
+            printf("  x(%4u) = %8.4f + j*%8.4f;\n", i, x[i].real, x[i].imag);
+
+        printf("y(t) :\n");
+        for (auto i=0; i<num_samples; i++) {
+            printf("  y(%4u) = %8.4f + j*%8.4f;", i, y[i].real, y[i].imag);
+            if ( (i >= k*m) && ((i%k)==0))
+                printf(" **\n");
+            else
+                printf("\n");
+        }
+
+    }
+
+
+
+    //LIQUID
+    TEST(LIQUID, Resample) {
         float input_signal_sine_1hz_100[100] = {
             0, 0.0627905,  0.125333,  0.187381,   0.24869,  0.309017,  0.368125,   0.42578,
              0.481754,  0.535827,  0.587786,  0.637425,  0.684548,  0.728969,  0.770513,  0.809017,
@@ -77,7 +148,7 @@ namespace {
             std::cout << input_signal_sine_1hz_100[i] << std::endl;
         */
         float r           = 0.5f;   // resampling rate (output/input)
-        unsigned int m    = 13;     // resampling filter semi-length (filter delay)
+        unsigned int m    = 3;     // resampling filter semi-length (filter delay)
         float As          = 60.0f;  // resampling filter stop-band attenuation [dB]
         float bw          = 0.45f;  // resampling filter bandwidth
         unsigned int npfb = 64;     // number of filters in bank (timing resolution)
@@ -98,7 +169,7 @@ namespace {
 
         // Set values to zero
         memset(x,0,nx * sizeof(liquid_float_complex));
-        memset(y,0,nx * sizeof(liquid_float_complex));
+        memset(y,0,y_len * sizeof(liquid_float_complex));
 
         //Fill signal, complex form
         for (auto i = 0; i < 100; i++)
@@ -124,6 +195,7 @@ namespace {
         resamp_crcf_destroy(q);
 
     }
+#endif
 
 }
 
