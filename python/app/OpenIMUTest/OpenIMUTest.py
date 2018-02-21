3ef88230d02bf7a563f1a16d9112d52148224343
Dominic Létourneau <doumdi@gmail.com>
2018-02-21 16:20:36 -0500

    Dependencies working better

--- python/app/OpenIMUTest/OpenIMUTest.py
+++ python/app/OpenIMUTest/OpenIMUTest.py
@@ -2,10 +2,24 @@ from PyQt5.QtWidgets import QMainWindow, QWidget, QVBoxLayout
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtCore import Qt
 from Charts import IMUChartView
+
+# This is auto-generated from Qt .ui files
+from UI_MainWindow import Ui_MainWindow
+
+# This is auto-generated from Qt .qrc files
+import RC_core
+
 import sys
 
-
 class MainWindow(QMainWindow):
+    def __init__(self, parent=None):
+        super(QMainWindow,self).__init__(parent=parent)
+        self.UI = Ui_MainWindow()
+        self.UI.setupUi(self)
+        self.showMaximized()
+
+
+class MainWindow2(QMainWindow):
     def __init__(self, parent=None):
         super(QMainWindow, self).__init__(parent=parent)
         self.centralWidget = QWidget(self)
@@ -28,7 +42,7 @@ class MainWindow(QMainWindow):
 
 # Main
 if __name__ == '__main__':
-
+    RC_core.qInitResources()
     app = QApplication(sys.argv)
     window = MainWindow()
     sys.exit(app.exec_())
