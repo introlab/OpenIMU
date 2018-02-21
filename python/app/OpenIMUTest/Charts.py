3ab2104bc16816c48a7bb1f23458af081a83208e
Dominic Létourneau <doumdi@gmail.com>
2018-02-21 15:42:21 -0500

    Generate .ui and .qrc file PyQt5 python files

--- python/app/OpenIMUTest/Charts.py
+++ python/app/OpenIMUTest/Charts.py
@@ -9,7 +9,8 @@ a high number of points, using OpenGL accelerated series
 """
 from PyQt5.QtGui import QPolygonF, QPainter
 from PyQt5.QtChart import QChart, QChartView, QLineSeries, QLegend
-
+from PyQt5.QtCore import Qt
+import numpy as np
 
 class IMUChartView(QChartView):
     def __init__(self, parent=None):
@@ -17,19 +18,24 @@ class IMUChartView(QChartView):
 
         self.chart = QChart()
         self.setChart(self.chart)
-        #self.chart.legend().hide()
+        self.chart.legend().setVisible(True)
+        self.chart.legend().setAlignment(Qt.AlignBottom)
         self.ncurves = 0
         self.setRenderHint(QPainter.Antialiasing)
 
-    def add_data(self, xdata, ydata, color=None):
+    def add_data(self, xdata, ydata, color=None, legend_text=None):
         curve = QLineSeries()
         pen = curve.pen()
         if color is not None:
             pen.setColor(color)
-        pen.setWidthF(.1)
+        pen.setWidthF(.4)
         curve.setPen(pen)
+
         curve.setUseOpenGL(True)
         curve.append(self.series_to_polyline(xdata, ydata))
+
+        #if legend_text is not None:
+
         self.chart.addSeries(curve)
         self.chart.createDefaultAxes()
         self.ncurves += 1
@@ -53,33 +59,32 @@ class IMUChartView(QChartView):
         memory[1:(size-1)*2+2:2] = ydata
         return polyline
 
+    def add_test_data(self):
+
+        # 100Hz, one day accelerometer values
+        npoints = 100 * 60 * 24
+
+        xdata = np.linspace(0., 10., npoints)
+        self.add_data(xdata, np.sin(xdata), color=Qt.red)
+        self.add_data(xdata, np.cos(xdata), color=Qt.green)
+        self.add_data(xdata, np.cos(2 * xdata), color=Qt.blue)
+        self.set_title("Simple example with %d curves of %d points " \
+                          "(OpenGL Accelerated Series)" \
+                          % (self.ncurves, npoints))
+
 
 # Testing app
 if __name__ == '__main__':
     import sys
-    import numpy as np
+
     from PyQt5.QtWidgets import QApplication
     from PyQt5.QtWidgets import QMainWindow, QPushButton
     from PyQt5.QtCore import Qt
 
     app = QApplication(sys.argv)
     window = QMainWindow()
-    button = QPushButton(window)
-    button.setText('hello world')
     imuView = IMUChartView(window)
-
-    #100Hz, one day accelerometer values
-    npoints = 100 * 60 * 24
-
-    xdata = np.linspace(0., 10., npoints)
-    imuView.add_data(xdata, np.sin(xdata), color=Qt.red)
-    imuView.add_data(xdata, np.cos(xdata), color=Qt.green)
-    imuView.add_data(xdata, np.cos(2 * xdata), color=Qt.blue)
-    imuView.set_title("Simple example with %d curves of %d points "\
-                     "(OpenGL Accelerated Series)"\
-                     % (imuView.ncurves, npoints))
-
-    imuView.set_title('IMUView demo')
+    imuView.add_test_data()
     window.setCentralWidget(imuView)
     window.setWindowTitle("IMUChartView Demo")
     window.resize(640,480)
