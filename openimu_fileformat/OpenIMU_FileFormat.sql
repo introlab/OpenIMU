88f72b0a3beed5005bec48b18bbc408743d63b6d
Simon Brière <simon.briere@usherbrooke.ca>
2018-03-19 11:40:18 -0400

    Added AutoIncrement to ID fields in OpenIMU file format structure.

--- openimu_fileformat/OpenIMU_FileFormat.sql
+++ openimu_fileformat/OpenIMU_FileFormat.sql
@@ -1,56 +1,51 @@
 
 CREATE TABLE tabGroups (
-                id_group BIGINT NOT NULL,
+                id_group INTEGER PRIMARY KEY AUTOINCREMENT,
                 name VARCHAR NOT NULL,
-                desc VARCHAR NOT NULL,
-                CONSTRAINT tabGroups_pk PRIMARY KEY (id_group)
+                desc VARCHAR NOT NULL
 );
 
 
 CREATE TABLE tabParticipants (
-                id_participant BIGINT NOT NULL,
+                id_participant INTEGER PRIMARY KEY  AUTOINCREMENT,
                 id_group BIGINT,
                 name VARCHAR NOT NULL,
                 desc VARCHAR,
-                CONSTRAINT tabParticipants_pk PRIMARY KEY (id_participant),
 				FOREIGN KEY (id_group) REFERENCES tabGroups (id_group) ON DELETE CASCADE ON UPDATE NO ACTION NOT DEFERRABLE
 );
 
 
 CREATE TABLE tabRecordsets (
-                id_recordset BIGINT NOT NULL,
+                id_recordset INTEGER PRIMARY KEY AUTOINCREMENT,
                 id_participant BIGINT NOT NULL,
                 name VARCHAR NOT NULL,
                 recordset_timestamp TIMESTAMP NOT NULL,
-                CONSTRAINT tabRecordsets_pk PRIMARY KEY (id_recordset),
 				FOREIGN KEY (id_participant) REFERENCES tabParticipants (id_participant) ON DELETE CASCADE ON UPDATE NO ACTION NOT DEFERRABLE
 );
 
 
 CREATE TABLE tabSubrecords (
-                id_subrecord BIGINT NOT NULL,
+                id_subrecord INTEGER PRIMARY KEY AUTOINCREMENT,
                 id_recordset BIGINT NOT NULL,
                 name VARCHAR NOT NULL,
                 bout_start_timestamp TIMESTAMP NOT NULL,
                 bout_end_timestamp TIMESTAMP NOT NULL,
-                CONSTRAINT tabSubrecords_pk PRIMARY KEY (id_subrecord)
 				FOREIGN KEY (id_recordset) REFERENCES tabRecordsets (id_recordset) ON DELETE CASCADE ON UPDATE NO ACTION NOT DEFERRABLE
 );
 
 
 CREATE TABLE tabDataProcessors (
-                id_data_processor BIGINT NOT NULL,
+                id_data_processor INTEGER PRIMARY KEY AUTOINCREMENT,
                 name VARCHAR NOT NULL,
                 unique_id VARCHAR NOT NULL,
                 version FLOAT NOT NULL,
                 author VARCHAR NOT NULL,
-                custom_script LONGVARCHAR NOT NULL,
-                CONSTRAINT tabDataProcessors_pk PRIMARY KEY (id_data_processor)
+                custom_script LONGVARCHAR NOT NULL
 );
 
 
 CREATE TABLE tabProcessedData (
-                id_processed_data BIGINT NOT NULL,
+                id_processed_data INTEGER PRIMARY KEY AUTOINCREMENT,
                 tabBouts_id_bout BIGINT NOT NULL,
                 id_recordset BIGINT,
                 id_data_processor BIGINT NOT NULL,
@@ -58,7 +53,6 @@ CREATE TABLE tabProcessedData (
                 name VARCHAR NOT NULL,
                 data BLOB NOT NULL,
                 data_timestamp TIMESTAMP NOT NULL,
-                CONSTRAINT tabProcessedData_pk PRIMARY KEY (id_processed_data),
 				FOREIGN KEY (id_recordset) REFERENCES tabRecordsets (id_recordset) ON DELETE CASCADE ON UPDATE NO ACTION NOT DEFERRABLE,
 				FOREIGN KEY (tabBouts_id_bout) REFERENCES tabSubrecords (id_subrecord) ON DELETE CASCADE ON UPDATE NO ACTION NOT DEFERRABLE,
 				FOREIGN KEY (id_data_processor) REFERENCES tabDataProcessors (id_data_processor) ON DELETE CASCADE ON UPDATE NO ACTION NOT DEFERRABLE
@@ -75,21 +69,19 @@ CREATE TABLE tabDataSet (
 
 
 CREATE TABLE tabDataFormat (
-                id_data_format BIGINT NOT NULL,
+                id_data_format INTEGER PRIMARY KEY AUTOINCREMENT,
                 name VARCHAR NOT NULL,
                 precision INTEGER NOT NULL,
-                floating_point BOOLEAN NOT NULL,
-                CONSTRAINT tabDataFormat_pk PRIMARY KEY (id_data_format)
+                floating_point BOOLEAN NOT NULL
 );
 
 
 CREATE TABLE tabSensorsTypes (
-                id_sensor_type BIGINT NOT NULL,
+                id_sensor_type INTEGER PRIMARY KEY NOT NULL,
                 name VARCHAR NOT NULL,
                 desc VARCHAR NOT NULL,
                 channels INTEGER NOT NULL,
-                display_style INTEGER NOT NULL,
-                CONSTRAINT tabSensorsTypes_pk PRIMARY KEY (id_sensor_type)
+                display_style INTEGER NOT NULL
 );
 
 
@@ -109,7 +101,7 @@ CREATE TABLE tabSensors (
 
 
 CREATE TABLE tabNotebook (
-                id_note BIGINT NOT NULL,
+                id_note INTEGER PRIMARY KEY AUTOINCREMENT,
                 tabProcessedData_id_processed_data BIGINT,
                 id_sensor BIGINT,
                 id_processed_data BIGINT,
@@ -120,7 +112,6 @@ CREATE TABLE tabNotebook (
                 id_group BIGINT,
                 id_data_processed BIGINT,
                 note VARCHAR NOT NULL,
-                CONSTRAINT tabNotebook_pk PRIMARY KEY (id_note),
 				FOREIGN KEY (id_group) REFERENCES tabGroups (id_group) ON DELETE CASCADE ON UPDATE NO ACTION NOT DEFERRABLE,
 				FOREIGN KEY (id_participant) REFERENCES tabParticipants (id_participant) ON DELETE CASCADE ON UPDATE NO ACTION NOT DEFERRABLE,
 				FOREIGN KEY (id_recordset) REFERENCES tabRecordsets (id_recordset) ON DELETE CASCADE ON UPDATE NO ACTION NOT DEFERRABLE,
@@ -133,21 +124,19 @@ CREATE TABLE tabNotebook (
 
 
 CREATE TABLE tabCalibration (
-                id_calibration BIGINT NOT NULL,
+                id_calibration INTEGER PRIMARY KEY AUTOINCREMENT,
                 id_sensor BIGINT NOT NULL,
                 data BLOB NOT NULL,
-                CONSTRAINT tabCalibration_pk PRIMARY KEY (id_calibration),
 				FOREIGN KEY (id_sensor) REFERENCES tabSensors (id_sensor) ON DELETE CASCADE ON UPDATE NO ACTION NOT DEFERRABLE
 );
 
 
 CREATE TABLE tabSensorsData (
-                id_sensor_data BIGINT NOT NULL,
+                id_sensor_data INTEGER PRIMARY KEY AUTOINCREMENT,
                 id_recordset BIGINT NOT NULL,
                 id_sensor BIGINT NOT NULL,
                 data_timestamp TIMESTAMP NOT NULL,
                 data BLOB NOT NULL,
-                CONSTRAINT tabSensorsData_pk PRIMARY KEY (id_sensor_data)
 				FOREIGN KEY (id_recordset) REFERENCES tabRecordsets (id_recordset) ON DELETE CASCADE ON UPDATE NO ACTION NOT DEFERRABLE,
 				FOREIGN KEY (id_sensor) REFERENCES tabSensors (id_sensor) ON DELETE CASCADE ON UPDATE NO ACTION NOT DEFERRABLE
 );
