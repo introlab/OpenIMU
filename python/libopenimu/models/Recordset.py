f3c25507bdbd82dbfe270f9b3614b789481ea6e2
Dominic Létourneau <doumdi@gmail.com>
2018-04-05 10:38:42 -0400

    Added Recordset

--- python/libopenimu/models/Recordset.py
+++ python/libopenimu/models/Recordset.py
@@ -1,10 +1,66 @@
 """
  Recordset
  @authors Simon Brière, Dominic Létourneau
- @date 27/03/2018
+ @date 05/04/2018
 """
+
+from libopenimu.models.Participant import Participant
 
 
 class Recordset:
-    def __init__(self):
-        return
+    def __init__(self, *args, **kwargs):
+        # Initialize from kwargs (and default values)
+        self._id_recordset = kwargs.get('id_recordset', None)
+        self._participant = kwargs.get('participant', Participant())
+        self._start_timestamp = kwargs.get('start_timestamp', 0)
+        self._end_timestamp = kwargs.get('end_timestamp', 0)
+
+        for arg in args:
+            if isinstance(arg,tuple):
+                self.from_tuple(arg)
+            elif isinstance(arg, Recordset):
+                self.from_tuple(arg.as_tuple())
+
+    def __str__(self):
+        return 'Recordset: ' + str(self.as_tuple())
+
+    def __eq__(self, other):
+        return self.as_tuple() == other.as_tuple()
+
+    def get_id_recordset(self):
+        return self._id_recordset
+
+    def set_id_recordset(self, id_recordset):
+        self._id_recordset = id_recordset
+
+    def get_participant(self):
+        return self._participant
+
+    def set_participant(self, participant):
+        self._participant = participant
+
+    def get_start_timestamp(self):
+        return self._start_timestamp
+
+    def set_start_timestamp(self, start_timestamp):
+        self._start_timestamp = start_timestamp
+
+    def get_end_timestamp(self):
+        return self._end_timestamp
+
+    def set_end_timestamp(self, end_timestamp):
+        self._end_timestamp = end_timestamp
+
+    def as_tuple(self):
+        return self._id_recordset, self._participant.as_tuple(), self._start_timestamp, self._end_timestamp
+
+    def from_tuple(self, t):
+        (self._id_recordset, participant_tuple, self._start_timestamp, self._end_timestamp) = t
+        self._participant.from_tuple(participant_tuple)
+
+    # Properties
+    id_recordset = property(get_id_recordset, set_id_recordset)
+    participant = property(get_participant, set_participant)
+    start_timestamp = property(get_start_timestamp, set_start_timestamp)
+    end_timestamp = property(get_end_timestamp, set_end_timestamp)
+
