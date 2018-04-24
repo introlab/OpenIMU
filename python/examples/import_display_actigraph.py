0b65cf8f3f1f3aa25b11856153e21509bdf65f9a
Dominic Létourneau <doumdi@gmail.com>
2018-04-24 08:39:28 -0400

    timing function, code cleanup

--- python/examples/import_display_actigraph.py
+++ python/examples/import_display_actigraph.py
@@ -1,21 +1,24 @@
 from libopenimu.importers.ActigraphImporter import ActigraphImporter
 from libopenimu.db.DBManager import DBManager
-from libopenimu.models.Recordset import  Recordset
 from libopenimu.models.sensor_types import SensorType
 from libopenimu.qt.Charts import IMUChartView
+from libopenimu.tools.timing import timing
 
 import os
 import numpy as np
 
+db_filename = 'actigraph.db'
 
+@timing
 def import_data_from_actigraph_file(filename):
     # This will create the database (or overwrite it)s
-    importer = ActigraphImporter('actigraph.db')
+    importer = ActigraphImporter(db_filename)
     # Load content of the file to the database
     results = importer.load(filename)
     importer.import_to_database(results)
 
 
+@timing
 def create_data_timeseries(sensor_data_list: list):
 
     time_values = []
@@ -29,14 +32,14 @@ def create_data_timeseries(sensor_data_list: list):
         time_values.append(vals['time'])
         data_values.append(vals['values'])
 
-    print('time_values length', len(time_values))
-    print('data_values length', len(data_values))
+    # print('time_values length', len(time_values))
+    # print('data_values length', len(data_values))
 
     # Concat vectors
     time_array = np.concatenate(time_values)
     data_array = np.concatenate(data_values)
 
-    print('time_array_shape, data_array_shape', time_array.shape, data_array.shape)
+    # print('time_array_shape, data_array_shape', time_array.shape, data_array.shape)
     # return data
     return {'x': time_array, 'y': data_array}
 
@@ -55,48 +58,40 @@ if __name__ == '__main__':
     timeseries_batt = []
     timeseries_lux = []
 
-    if not os.path.isfile('actigraph.db'):
+    if not os.path.isfile(db_filename):
         print('importing actigraph data')
         import_data_from_actigraph_file('../resources/samples/test.gt3x')
 
     # manager will handle the newly created database
-    manager = DBManager('actigraph.db', overwrite=False)
+    manager = DBManager(db_filename, overwrite=False)
 
     # Let's load all the recordsets (should have at least one)
     recordsets = manager.get_all_recordsets()
 
     for record in recordsets:
-        print('Processing record', record)
-
         # Get all sensors in record
         sensors = manager.get_all_sensors()
         for sensor in sensors:
-            print('Processing sensor', sensor)
             if sensor.id_sensor_type == SensorType.ACCELEROMETER:
                 print('Found Accelerometer')
                 channels = manager.get_all_channels(sensor=sensor)
                 for channel in channels:
-                    print('Found Channel (acc)', channel)
-
+                    print('Found Channel (accelerometer)', channel)
                     # Will get all data (converted to floats)
                     channel_data = manager.get_all_sensor_data(recordset=record, convert=True, sensor=sensor,
                                                                channel=channel)
-
-                    print('Channel data len', len(channel_data))
                     timeseries_acc.append(create_data_timeseries(channel_data))
                     timeseries_acc[-1]['label'] = channel.label
             if sensor.id_sensor_type == SensorType.BATTERY:
-                print('Found Battery')
                 channels = manager.get_all_channels(sensor=sensor)
                 for channel in channels:
-                    print('Found Channel (batt)', channel)
+                    print('Found Channel (battery)', channel)
                     channel_data = manager.get_all_sensor_data(recordset=record, convert=True, sensor=sensor,
                                                                channel=channel)
                     timeseries_batt.append(create_data_timeseries(channel_data))
                     timeseries_batt[-1]['label'] = channel.label
 
             if sensor.id_sensor_type == SensorType.LUX:
-                print('Found Lux')
                 channels = manager.get_all_channels(sensor=sensor)
                 for channel in channels:
                     print('Found Channel (lux)', channel)
