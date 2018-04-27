d69dd62d828d3bc901c2c521b5fea38b1a702ef8
Simon Briere <simon.briere@usherbrooke.ca>
2018-04-27 11:48:25 -0400

    Participant adding, editing and deleting added. TODO: Deleting a group doesnt seem to cascade deletes to participants...

--- python/OpenIMUApp.py
+++ python/OpenIMUApp.py
@@ -76,6 +76,8 @@ class MainWindow(QMainWindow):
         self.add_to_log("Chargement des données...", LogTypes.LOGTYPE_INFO)
         self.currentDataSet = self.dbMan.get_dataset()
         self.load_data_from_dataset()
+        self.UI.treeDataSet.setCurrentItem(None)
+
         # self.loadDemoData()
         self.add_to_log("Données chargées!", LogTypes.LOGTYPE_DONE)
 
@@ -83,26 +85,33 @@ class MainWindow(QMainWindow):
         self.UI.treeDataSet.itemClicked.connect(self.tree_item_clicked)
         self.UI.btnDataSetInfos.clicked.connect(self.infosRequested)
         self.UI.btnAddGroup.clicked.connect(self.newGroupRequested)
+        self.UI.btnAddParticipant.clicked.connect(self.newParticipantRequested)
+        self.UI.treeDataSet.participantDragged.connect(self.participant_was_dragged)
         self.UI.btnDelete.clicked.connect(self.delete_requested)
 
     def load_data_from_dataset(self):
         # Groups
         groups = self.dbMan.get_all_groups()
         for group in groups:
-            item = self.UI.treeDataSet.add_group(group)
+            self.UI.treeDataSet.update_group(group)
+            participants = self.dbMan.get_participants_for_group(group)
+            for participant in participants:
+                self.UI.treeDataSet.update_participant(participant)
+
+        # Participants without a group
+        participants = self.dbMan.get_participants_for_group(None)
+        for participant in participants:
+            self.UI.treeDataSet.update_participant(participant)
 
     def loadDemoData(self):
         # Load demo structure
         new_group = Group(id_group=0, name="Groupe 1", description="Ceci est un premier groupe de démonstration")
 
-        item = self.UI.treeDataSet.add_group(new_group)
+        item = self.UI.treeDataSet.update_group(new_group)
 
-        parent = item
         new_part = Participant(id_participant=1, name="Participant B", description="Participant dans un groupe",
                                group=new_group)
-        item = self.create_participant_item(new_part)
-        parent.addChild(item)
-        self.UI.treeDataSet.participants[new_part.id_participant] = new_part
+        item = self.UI.treeDataSet.update_participant(new_part)
 
         item2 = self.create_records_item()
         item.addChild(item2)
@@ -157,9 +166,7 @@ class MainWindow(QMainWindow):
         item2.addChild(item3)
 
         new_part = Participant(id_participant=2, name="Participant C", group=new_group)
-        item = self.create_participant_item(new_part)
-        parent.addChild(item)
-        self.UI.treeDataSet.participants[new_part.id_participant] = new_part
+        item = self.UI.treeDataSet.update_participant(new_part)
 
         parent = item
         item = self.create_records_item()
@@ -170,28 +177,14 @@ class MainWindow(QMainWindow):
 
         new_group = Group(id_group=1, name="Groupe 2", description="Ceci est un deuxième groupe de démonstration")
 
-        item = self.UI.treeDataSet.add_group(new_group)
+        item = self.UI.treeDataSet.update_group(new_group)
 
         parent = item
         new_part = Participant(id_participant=3, name="Participant D", group=new_group)
-        item = self.create_participant_item(new_part)
-        parent.addChild(item)
-        self.UI.treeDataSet.participants[new_part.id_participant] = new_part
+        item = self.UI.treeDataSet.update_participant(new_part)
 
         new_part = Participant(id_participant=0, name="Participant A", description="Participant sans groupe")
-        item = self.create_participant_item(new_part)
-        self.UI.treeDataSet.participants[new_part.id_participant] = new_part
-        self.UI.treeDataSet.addTopLevelItem(item)
-
-
-    def create_participant_item(self, part):
-        item = QTreeWidgetItem()
-        item.setText(0, part.name)
-        item.setIcon(0, QIcon(':/OpenIMU/icons/participant.png'))
-        item.setData(0, Qt.UserRole, part.id_participant)
-        item.setData(1, Qt.UserRole, 'participant')
-        item.setFont(0, QFont('Helvetica', 13, QFont.Bold))
-        return item
+        item = self.UI.treeDataSet.update_participant(new_part)
 
     def create_records_item(self):
         item = QTreeWidgetItem()
@@ -246,20 +239,11 @@ class MainWindow(QMainWindow):
         return item
 
     def update_group(self, group):
-        # Find the group in the treelist
-        if group.id_group in self.UI.treeDataSet.groups.keys():
-            self.UI.treeDataSet.groups[group.id_group] = group
-            for i in range(0, self.UI.treeDataSet.topLevelItemCount()):
-                item = self.UI.treeDataSet.topLevelItem(i)
-                if self.UI.treeDataSet.get_item_id(item) == group.id_group:
-                    item.setText(0, group.name)
-                    break
+        item = self.UI.treeDataSet.update_group(group)
+        self.UI.treeDataSet.setCurrentItem(item)
 
-        else:
-            item = self.create_group_item(group)
-            self.UI.treeDataSet.addTopLevelItem(item)
-            self.UI.treeDataSet.groups[group.id_group] = group
-
+    def update_participant(self, participant):
+        item = self.UI.treeDataSet.update_participant(participant)
         self.UI.treeDataSet.setCurrentItem(item)
 
     def clear_main_widgets(self):
@@ -274,6 +258,15 @@ class MainWindow(QMainWindow):
 
         groupWidget.dataSaved.connect(self.dataWasSaved)
         groupWidget.dataCancelled.connect(self.dataWasCancelled)
+
+    def show_participant(self, participant=None, base_group=None):
+        self.clear_main_widgets()
+
+        partWidget = ParticipantWindow(dbManager=self.dbMan, participant=participant, default_group=base_group)
+        self.UI.frmMain.layout().addWidget(partWidget)
+
+        partWidget.dataSaved.connect(self.dataWasSaved)
+        partWidget.dataCancelled.connect(self.dataWasCancelled)
 
     def add_to_log(self, text, log_type):
         format = ""
@@ -315,6 +308,25 @@ class MainWindow(QMainWindow):
     def newGroupRequested(self):
         self.show_group()
 
+    @pyqtSlot()
+    def newParticipantRequested(self):
+        # Check if we can get a root item (group) for the current selected item or not
+        item = self.UI.treeDataSet.currentItem()
+        if item is not None:
+            while item.parent() is not None:
+                item = item.parent()
+
+        default_group = None
+        if self.UI.treeDataSet.get_item_type(item) == "group":
+            default_group = self.UI.treeDataSet.groups[self.UI.treeDataSet.get_item_id(item)]
+
+        self.show_participant(base_group=default_group)
+
+    @pyqtSlot(Participant)
+    def participant_was_dragged(self,participant):
+        self.dbMan.update_participant(participant)
+        self.update_participant(participant)
+
     @pyqtSlot(QTreeWidgetItem, int)
     def tree_item_clicked(self, item, column):
         # print(item.text(column))
@@ -330,8 +342,7 @@ class MainWindow(QMainWindow):
             # self.UI.frmMain.layout().addWidget(groupWidget)
 
         if item_type == "participant":
-            partWidget = ParticipantWindow(participant=self.UI.treeDataSet.participants[item_id])
-            self.UI.frmMain.layout().addWidget(partWidget)
+            self.show_participant(self.UI.treeDataSet.participants[item_id])
 
         if item_type == "recordsets" or item_type == "recordset" or item_type == "subrecord":
             recordsWidget = RecordsetWindow()
@@ -352,12 +363,22 @@ class MainWindow(QMainWindow):
             self.update_group(group)
             self.add_to_log("Groupe " + group.name + " mis à jour.", LogTypes.LOGTYPE_DONE)
 
+        if item_type == "participant":
+            part = self.UI.frmMain.layout().itemAt(0).widget().participant
+            self.update_participant(part)
+            self.add_to_log("Participant " + part.name + " mis à jour.", LogTypes.LOGTYPE_DONE)
+
+
     @pyqtSlot()
     def dataWasCancelled(self):
         item_type = self.get_current_widget_data_type()
 
         if item_type == "group":
             if self.UI.frmMain.layout().itemAt(0).widget().group is None:
+                self.clear_main_widgets()
+
+        if item_type == "participant":
+            if self.UI.frmMain.layout().itemAt(0).widget().participant is None:
                 self.clear_main_widgets()
 
     @pyqtSlot()
@@ -373,11 +394,19 @@ class MainWindow(QMainWindow):
 
         rval = msg.exec()
         if rval == QMessageBox.Yes:
+            item_name = self.UI.treeDataSet.currentItem().text(0)
             if item_type == "group":
                 group = self.UI.treeDataSet.groups[item_id]
                 self.dbMan.delete_group(group)
                 self.UI.treeDataSet.remove_group(group)
 
+            if item_type == "participant":
+                part = self.UI.treeDataSet.participants[item_id]
+                self.dbMan.delete_participant(part)
+                self.UI.treeDataSet.remove_participant(part)
+
+            self.add_to_log(item_name + " a été supprimé.", LogTypes.LOGTYPE_DONE)
+            self.clear_main_widgets()
 
     def closeEvent(self, event):
         print('closeEvent')
@@ -401,34 +430,111 @@ class Treedatawidget(QTreeWidget):
     groups = {}
     participants = {}
 
+    items_groups = {}
+    items_participants = {}
+
+    participantDragged = pyqtSignal(Participant)
+
     def __init__(self, parent=None):
         super(QTreeWidget, self).__init__(parent=parent)
 
     def remove_group(self,group):
+        item = self.items_groups.get(group.id_group, None)
         for i in range(0, self.topLevelItemCount()):
-            if self.get_item_id(self.topLevelItem(i)) == group.id_group:
+            if self.topLevelItem(i) == item:
                 self.takeTopLevelItem(i)
                 self.groups[group.id_group] = None
+                self.items_groups[group.id_group] = None
                 break
 
-    def add_group(self, group):
-        item = QTreeWidgetItem()
-        item.setText(0, group.name)
-        item.setIcon(0, QIcon(':/OpenIMU/icons/group.png'))
-        item.setData(0, Qt.UserRole, group.id_group)
-        item.setData(1, Qt.UserRole, 'group')
-        item.setFont(0, QFont('Helvetica', 14, QFont.Bold))
+    def remove_participant(self,participant):
+        item = self.items_participants.get(participant.id_participant, None)
+        if participant.id_group is None: # Participant without a group
+            for i in range(0, self.topLevelItemCount()):
+                if self.topLevelItem(i) == item:
+                    self.takeTopLevelItem(i)
+                    break
+        else:
+            for i in range(0, item.parent().childCount()):
+                if item.parent().child(i) == item:
+                    item.parent().takeChild(i)
+                    break
 
-        self.addTopLevelItem(item)
-        self.groups[group.id_group] = group
+        self.participants[participant.id_participant] = None
+        self.items_participants[participant.id_participant] = None
+
+    def update_group(self, group):
+        item = self.items_groups.get(group.id_group, None)
+        if item is None:
+            item = QTreeWidgetItem()
+            item.setText(0, group.name)
+            item.setIcon(0, QIcon(':/OpenIMU/icons/group.png'))
+            item.setData(0, Qt.UserRole, group.id_group)
+            item.setData(1, Qt.UserRole, 'group')
+            item.setFont(0, QFont('Helvetica', 14, QFont.Bold))
+
+            self.addTopLevelItem(item)
+            self.groups[group.id_group] = group
+            self.items_groups[group.id_group] = item
+        else:
+            item.setText(0, group.name)
 
         return item
 
+    def update_participant(self, part):
+        item = self.items_participants.get(part.id_participant, None)
+        group_item = self.items_groups.get(part.id_group, None)
+        if item is None:
+            item = QTreeWidgetItem()
+            item.setText(0, part.name)
+            item.setIcon(0, QIcon(':/OpenIMU/icons/participant.png'))
+            item.setData(0, Qt.UserRole, part.id_participant)
+            item.setData(1, Qt.UserRole, 'participant')
+            item.setFont(0, QFont('Helvetica', 13, QFont.Bold))
+
+            if group_item is None: #Participant without a group
+                self.addTopLevelItem(item)
+            else:
+                group_item.addChild(item)
+
+        else:
+            item.setText(0, part.name)
+            # Check if we must move it or not, if the group changed
+            if item.parent() != group_item:
+                # Old group - find and remove current item
+                if item.parent() is None: # No parent...
+                    for i in range(0, self.topLevelItemCount()):
+                        if self.topLevelItem(i) == item:
+                            item = self.takeTopLevelItem(i)
+                            break
+                else:
+                    # Had a group...
+                    for i in range(0, item.parent().childCount()):
+                        if item.parent().child(i) == item:
+                            item = item.parent().takeChild(i)
+                            break
+
+                # New group
+                if group_item is None:  # Participant without a group
+                    self.addTopLevelItem(item)
+                else:
+                    group_item.addChild(item)
+
+        self.participants[part.id_participant] = part
+        self.items_participants[part.id_participant] = item
+        return item
+
     def get_item_type(self,item):
-        return item.data(1, Qt.UserRole)
+        if item is not None:
+            return item.data(1, Qt.UserRole)
+        else:
+            return ""
 
     def get_item_id(self,item):
-        return item.data(0, Qt.UserRole)
+        if item is not None:
+            return item.data(0, Qt.UserRole)
+        else:
+            return ""
 
     def dropEvent(self, event):
 
@@ -448,16 +554,20 @@ class Treedatawidget(QTreeWidget):
             if not index.isValid():
                 # Clear source and set to no group
                 self.participants[source_id].group = None
-                new_item = source_item.clone()
-                self.addTopLevelItem(new_item)
+                self.participants[source_id].id_group = None
+                #new_item = source_item.clone()
+                #self.addTopLevelItem(new_item)
+                self.participantDragged.emit(self.participants[source_id])
                 event.accept()
                 return
             else:
 
                 if target_type == "group":
                     self.participants[source_id].group = self.groups[target_id]
-                    new_item = source_item.clone()
-                    target_item.addChild(new_item)
+                    self.participants[source_id].id_group = self.groups[target_id].id_group
+                    #new_item = source_item.clone()
+                    #target_item.addChild(new_item)
+                    self.participantDragged.emit(self.participants[source_id])
                     event.accept()
                     return
 
--- python/libopenimu/db/DBManager.py
+++ python/libopenimu/db/DBManager.py
@@ -108,22 +108,52 @@ class DBManager:
         # print('all groups', query.all())
         return query.all()
 
-    #####################
-    def add_participant(self, group: Group, name, description):
-        # Create object
-        participant = Participant(group=group, name=name, description=description)
-        self.session.add(participant)
-        self.commit()
-        return participant
+    ######## PARTICIPANTS
+    def update_participant(self, participant):
+        try:
+            if participant.id_participant is None:
+                self.session.add(participant)
+            else:
+                src_part = self.session.query(Participant).filter(Participant.id_participant == participant.id_participant).first()
+                src_part.name = participant.name
+                src_part.description = participant.description
+                src_part.id_group = participant.id_group
+                participant.id_participant = src_part.id_participant
+
+            self.commit()
+            return participant
+
+        except Exception as e:
+            message = 'Error updating participant' + ': ' + str(e)
+            print('Error: ', message)
+            raise
 
     def get_participant(self, id_participant):
         query = self.session.query(Participant).filter(Participant.id_participant == id_participant)
         return query.first()
 
-    def get_all_participants(self):
+    """def get_all_participants(self):
         query = self.session.query(Participant)
         return query.all()
+    """
 
+    def get_participants_for_group(self,group):
+        if group is not None:
+            query = self.session.query(Participant).filter(Participant.id_group == group.id_group)
+        else:
+            query = self.session.query(Participant).filter(Participant.id_group == None)
+        return query.all()
+
+    def delete_participant(self, part):
+        try:
+            self.session.delete(part)
+            self.commit()
+        except Exception as e:
+            message = 'Error deleting participant' + ': ' + str(e)
+            print('Error: ', message)
+            raise
+
+    #####################
     def add_sensor(self, _id_sensor_type, _name, _hw_name, _location, _sampling_rate, _data_rate):
         # Create object
         sensor = Sensor(
