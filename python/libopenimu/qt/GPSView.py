260435231e022412d6fd363b2ca5afc94fa94894
doumdi <doumdi@gmail.com>
2018-04-28 15:39:02 -0400

    display basic map

--- python/libopenimu/qt/GPSView.py
+++ python/libopenimu/qt/GPSView.py
@@ -1,10 +1,34 @@
+import sys
 
-class GPSView:
-    def __init__(self):
-        
+from PyQt5.QtWebEngineWidgets import QWebEngineView
+from PyQt5.QtCore import QDir, QUrl
+# This will automatically load qrc
+import core_rc
+
+class GPSView(QWebEngineView):
+    def __init__(self, parent):
+        super(QWebEngineView, self).__init__(parent)
+
+        # Load file from qrc
+        self.setUrl(QUrl('qrc:/OpenIMU/html/map.html'))
 
 
 # Testing app
 if __name__ == '__main__':
-    print('Testing GPSView')
+
+    from PyQt5.QtWidgets import QApplication
+    from PyQt5.QtWidgets import QMainWindow, QPushButton
+    from PyQt5.QtCore import Qt
+
+    app = QApplication(sys.argv)
+
+    window = QMainWindow()
+    window.setWindowTitle('GPSView - Test')
+    window.resize(640, 480)
+
+    view = GPSView(window)
+    window.setCentralWidget(view)
+    window.show()
+
+    sys.exit(app.exec_())
 
