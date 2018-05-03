c1fcbdcb1ed2f82479bdbbc59606a06395566c01
Simon Briere <simon.briere@usherbrooke.ca>
2018-05-03 09:18:15 -0400

    Fixed cascade delete of groups & participants.

--- python/libopenimu/db/DBManager.py
+++ python/libopenimu/db/DBManager.py
@@ -9,6 +9,8 @@ import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.sql import table, insert
+from sqlalchemy.engine import Engine
+from sqlalchemy import event
 
 import os
 import datetime
@@ -57,6 +59,12 @@ class DBManager:
 
         # Session instance
         self.session = self.SessionMaker()
+
+    @event.listens_for(Engine, "connect")
+    def set_sqlite_pragma(dbapi_connection, connection_record):
+        cursor = dbapi_connection.cursor()
+        cursor.execute("PRAGMA foreign_keys=ON")
+        cursor.close()
 
     def commit(self):
         self.session.commit()
