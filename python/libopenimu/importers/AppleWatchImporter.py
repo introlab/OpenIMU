1b08e8794ac811fd8dfef330495c3fd3671d7813
Dominic Létourneau <doumdi@gmail.com>
2018-05-31 09:49:49 -0400

    reading binary data ok

--- python/libopenimu/importers/AppleWatchImporter.py
+++ python/libopenimu/importers/AppleWatchImporter.py
@@ -25,7 +25,9 @@ import sys
 import binascii
 import datetime
 import string
-
+import os
+import zipfile
+import struct
 
 class AppleWatchImporter(BaseImporter):
 
@@ -34,12 +36,12 @@ class AppleWatchImporter(BaseImporter):
     SENSORIA_ID = 0x02
     HEARTRATE_ID = 0x03
     MOTION_ID = 0x04
-    LOCATION_ID = 0x05
+    # LOCATION_ID = 0x05
     BEACONS_ID = 0x06
     COORDINATES_ID = 0x7
 
     def __init__(self, manager: DBManager, participant: Participant):
-        super(BaseImporter, self).__init__(manager, participant)
+        super().__init__(manager, participant)
         pass
 
     def load(self, filename):
@@ -51,6 +53,22 @@ class AppleWatchImporter(BaseImporter):
             results = self.readDataFile(file)
 
         return results
+
+    def load_zip(self, filename):
+        with zipfile.ZipFile(filename) as myzip:
+            print('zip opened')
+            namelist = myzip.namelist()
+
+            print('zip contains : ', namelist)
+
+            # First find SETTINGS file
+            for file in namelist:
+                if '.data' in file:
+                    my_file = myzip.open(file)
+                    self.readDataFile(my_file, True)
+                else:
+                    pass
+                    # print('Unknown file : ', file)
 
     def import_to_database(self, result):
         print('AppleWatchImporter.import_to_database')
@@ -80,14 +98,16 @@ class AppleWatchImporter(BaseImporter):
             return None
 
         if debug:
-            print('reading header : ', id, version, participant_id)
+            print('reading header : ', hex(id), hex(version), participant_id)
 
         while file.readable():
             # Read sensor ID
-            sensor_id = struct.unpack("<B", file.read(1))
+            [sensor_id] = struct.unpack("<B", file.read(1))
+
+            print('sensor_id : ', hex(sensor_id))
 
             # Read timestamp
-            timestamp_ms = struct.unpack("<Q", file.read(8))
+            [timestamp_ms] = struct.unpack("<Q", file.read(8))
             if debug:
                 print('TIMESTAMP: ', timestamp_ms)
 
@@ -99,7 +119,7 @@ class AppleWatchImporter(BaseImporter):
                 results[timestamp_ms]['sensoria'] = []
                 results[timestamp_ms]['heartrate'] = []
                 results[timestamp_ms]['motion'] = []
-                results[timestamp_ms]['location'] = []
+                # results[timestamp_ms]['location'] = []
                 results[timestamp_ms]['beacons'] = []
                 results[timestamp_ms]['coordinates'] = []
 
@@ -110,7 +130,7 @@ class AppleWatchImporter(BaseImporter):
 
             elif sensor_id == self.SENSORIA_ID:
                 # Sensoria data
-                data = self.read_sensoria_data(file.read(20), debug)
+                data = self.read_sensoria_data(file.read(42), debug)
                 results[timestamp_ms]['sensoria'].append(data)
 
             elif sensor_id == self.HEARTRATE_ID:
@@ -120,14 +140,14 @@ class AppleWatchImporter(BaseImporter):
 
             elif sensor_id == self.MOTION_ID:
                 # Motion data
-                data =  self.read_motion_data(file.read(104), debug)
+                data =  self.read_motion_data(file.read(52), debug)
                 results[timestamp_ms]['motion'].append(data)
 
-            elif sensor_id == self.LOCATION_ID:
-                # Location data
-                # ????
-                data = self.read_location_data(file.read(0), debug)
-                results[timestamp_ms]['location'].append(data)
+            # elif sensor_id == self.LOCATION_ID:
+            #    # Location data
+            #    # ????
+            #    data = self.read_location_data(file.read(0), debug)
+            #    results[timestamp_ms]['location'].append(data)
 
             elif sensor_id == self.BEACONS_ID:
                 # Beacons data
@@ -137,7 +157,7 @@ class AppleWatchImporter(BaseImporter):
             elif sensor_id == self.COORDINATES_ID:
                 # Coordinates data
                 # ??? (LOCATION ???)
-                data = self.read_coordinates_data(file.read(24), debug)
+                data = self.read_coordinates_data(file.read(28), debug)
                 results[timestamp_ms]['coordinates'].append(data)
 
             else:
@@ -165,10 +185,20 @@ class AppleWatchImporter(BaseImporter):
 
     def read_sensoria_data(self, chunk, debug=False):
         """
-        20 bytes of received frame (will require further processing)
+        42 bytes of received frame (will require further processing)
+        This assumes that all socks we have use the F20 streaming protocol, as defined in
+        SensoriaCoreData. As of now, the app ignores all other protocol to not pollute the files. In the
+        future, it would be the best to deserialize these frames directly upon importation into OpenIMU.
+        As this will be more cumbersome, for now the deserialization is made directly on reception.
+        • 3 Int16 for the 3 channels (pressure sensors)
+        • 9 Float32 for inertial sensors
+        ◦ accelerometer (x,y,z)
+        ◦ gyroscope (x,y,z)
+        ◦ magnetometer (x,y,z)
+
         """
-        assert(len(chunk) == 20)
-        data = struct.unpack("<20B", chunk)
+        assert(len(chunk) == 42)
+        data = struct.unpack("<3h9f", chunk)
         if debug:
             print('SENSORIA: ', data)
         return data
@@ -188,14 +218,14 @@ class AppleWatchImporter(BaseImporter):
 
     def read_motion_data(self, chunk, debug=False):
         """
-        13 Float64 values, hence 104 bytes, with following fields
+         13 Float32 values, hence 52 bytes, with following fields
         • acceleration (x,y,z)
         • gravity vector (x,y,z)
         • gyroscope (x,y,z)
         • attitude quaternion (w,x,y,z)
-        """
-        assert (len(chunk) == 104)
-        data = struct.unpack("<13d", chunk)
+            """
+        assert (len(chunk) == 52)
+        data = struct.unpack("<13f", chunk)
         if debug:
             print('MOTION: ', data)
         return data
@@ -219,15 +249,25 @@ class AppleWatchImporter(BaseImporter):
 
     def read_coordinates_data(self, chunk, debug=False):
         """
-        3 Float64 values, hence 24 bytes
+          7 Float32 values, hence 28 bytes
         • latitude
         • longitude
-        • accuracy in meters
-        :param chunk:
+        • accuracy in meters for lat/long
+        • altitude in meters from sea level
+        • accuracy in meters for altitude
+        • speed (meters per second)
+        • course (degrees relative to true north)
         :return:
         """
-        assert (len(chunk) == 24)
-        data = struct.unpack("<3d", chunk)
+        assert (len(chunk) == 28)
+        data = struct.unpack("<7f", chunk)
         if debug:
             print('COORDINATES: ', data)
         return data
+
+
+
+
+
+
+
--- python/libopenimu/importers/tests/test_AppleWatchImporter.py
+++ python/libopenimu/importers/tests/test_AppleWatchImporter.py
@@ -15,6 +15,7 @@ from libopenimu.importers.AppleWatchImporter import AppleWatchImporter
 from libopenimu.models.Participant import Participant
 from libopenimu.db.DBManager import DBManager
 
+
 class AppleWatchImporterTest(unittest.TestCase):
 
     def setUp(self):
@@ -23,3 +24,17 @@ class AppleWatchImporterTest(unittest.TestCase):
     def tearDown(self):
         pass
 
+    def test_load_file(self):
+        pass
+
+    def test_load_zip_file(self):
+        # Testing
+
+        manager = DBManager('applewatch.db', overwrite=True)
+        participant = Participant(name='My Participant', description='Participant Description')
+        manager.update_participant(participant)
+        importer = AppleWatchImporter(manager, participant)
+
+        results = importer.load_zip('../../../resources/samples/AppleWatch.zip')
+        # print('results', results)
+        # importer.import_to_database(results)
